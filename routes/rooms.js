608bcb9d21be6a398aaa01ee661a334fae17c16f
berlin-hash <chirashishjeena@gmail.com>
2022-04-22 23:33:38 +0530

    april22

--- routes/rooms.js
+++ routes/rooms.js
@@ -2,31 +2,32 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const path = require('path')
 const Room = require('../models/Room')
-const Member = require('../models/Member')
+const Member = require('../models/Member');
+const { render } = require('express/lib/response');
 const app = express();
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const router =express.Router();
-router.get('/',(req, res)=> {
+const router = express.Router();
+router.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname, '../templates/index.html'))
     res.render('home')
 })
 
-router.get('/createRoom', (req, res)=> {
+router.get('/createRoom', (req, res) => {
     res.render('createRoom')
 })
 
-router.post('/roomPost',async (req, res)=> {
+router.post('/roomPost', async (req, res) => {
     console.log(req.body);
     const room = new Room(req.body);
-    try{
+    try {
         const savedRoom = await room.save()
-       
+
         res.render('successfulCreation',)
     }
-    catch(err){
-        res.json({message: err});
+    catch (err) {
+        res.json({ message: err });
     }
 })
 
@@ -36,39 +37,48 @@ router.get('/joinRoom', (req, res) => {
     res.render('joinRoom')
 })
 
-router.post('/joinRoomData',async (req, res) => {
+router.post('/joinRoomData', async (req, res) => {
+    console.log(req.body.memBool);
     // console.log(req.body.rname);
     // const room = new Room(req.body);
-    try{
-        const room =await Room.findOne({rname: req.body.rname, rcode: req.body.rcode});
-        if(room == undefined)
-            res.render('../views/roomLogin/unsucLogin.handlebars', {rname: req.body.rname})
-        else{
-            const members = await Member.find({rname: req.body.rname})
+    if (parseInt(req.body.memBool)) {
+        console.log("hello");
+        addMember(req);
+    }
+
+    try {
+        const room = await Room.findOne({ rname: req.body.rname, rcode: req.body.rcode });
+        if (room == undefined)
+            res.render('../views/roomLogin/unsucLogin.handlebars', { rname: req.body.rname })
+        else {
+            const members = await Member.find({ rname: req.body.rname }).lean();
             console.log(members);
             data = [
-                {mname: 'Chir'},
-                {mname: 'Asw'},
-                {mname: 'Dru'}
+                { mname: 'Chir' },
+                { mname: 'Asw' },
+                { mname: 'Dru' }
             ]
-            res.render('../views/roomLogin/sucLogin.handlebars', {members: data, rname: req.body.rname})
+            res.render('../views/roomLogin/sucLogin.handlebars', { members: members, rname: req.body.rname, rcode: req.body.rcode })
         }
-            
+
     }
-    catch(err){
-        res.json({message: err})
+    catch (err) {
+        res.json({ message: err })
     }
+
+
 })
 
 
 //Add a member
-router.post('/addMember', async (req, res) => {
+function addMember(req) {
     const member = new Member(req.body)
     try {
-        const savedInfo = await member.save();
+        const savedInfo =  member.save();
     }
-    catch(err){
-        res.json({message: err})
+    catch (err) {
+        res.json({ message: err })
     }
-})
+
+}
 module.exports = router;
